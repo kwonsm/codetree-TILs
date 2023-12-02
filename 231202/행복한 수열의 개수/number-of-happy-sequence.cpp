b48ea9f7c9d918aa8567fb6e390895463a74e5c5
kwonsm <67861746+kwonsm@users.noreply.github.com>
2023-12-02 15:03:14 +0900

    Title: 행복한 수열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231202/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int sol(int n, int m, vector<vector<int>> map){
+    int ans=0;
+    for(auto s:map){
+        int temp=s[0];
+        int check=m;
+        for(auto a:s){
+            if(temp==a)
+                check--;
+            else{
+                check=m-1;
+                temp=a;
+            }
+            if(check==0) {
+                ans++;
+                continue;
+            }
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        int temp=map[0][i];
+        int check=m;
+        for(int j=0;j<n;j++){
+            if(temp==map[j][i])
+                check--;
+            else{
+                check=m-1;
+                temp=map[j][i];
+            }
+            if(check==0) {
+                ans++;
+                continue;
+            }
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    int n, m;
+    cin>> n>> m;
+    vector<vector<int>> map(n,vector<int>(n));
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+            cin>>map[i][j];
+    
+    cout<<sol(n,m,map)<<endl;
+    return 0;
+}
